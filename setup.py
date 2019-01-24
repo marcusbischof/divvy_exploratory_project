49116715b3e1346e61a435e16597036a5755e26b
Marcus Bischof <marcusbischof@Marcuss-MacBook-Pro.local>
2019-01-23 20:54:25 -0600

    Starting exploratory project for divvy data, sourced form Kaggle. File structure seed is cookiecutter data science project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An exploratory analysis of divvy data from kaggle. Data acquired from: https://www.kaggle.com/yingwurenjian/chicago-divvy-bicycle-sharing-data',
+    author='Marcus Bischof',
+    license='',
+)
